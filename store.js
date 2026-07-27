4a7f9560fbde0b36e90cc359b1a431386c6676bc
Lona Smith <71089848+Lona-web558@users.noreply.github.com>
2026-07-27 03:32:08 +0200

    Update store.js

--- store.js
+++ store.js
@@ -21,7 +21,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const DATA_DIR = path.join(__dirname, 'data');
+const DATA_DIR = path.join(__dirname);
 const COLLECTIONS = [
   'users', 'accounts', 'wallets', 'transactions',
   'transfers', 'loans', 'otps', 'events', 'fraudFlags'
